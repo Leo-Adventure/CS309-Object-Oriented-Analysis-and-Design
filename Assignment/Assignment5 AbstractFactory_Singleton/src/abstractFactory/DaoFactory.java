b6a4f5e86ecca1e3003abbd92909d4a63a2177b7
Leo_Adventure <12012919@mail.sustech.edu.cn>
2022-12-08 12:22:02 +0800

    upload Assignment 5 Abstract Factory combined with Singleton and Reflection

--- Assignment/Assignment5 AbstractFactory_Singleton/src/abstractFactory/DaoFactory.java
+++
@@ -1,9 +0,0 @@
-package abstractFactory;
-
-import dao.ComputerDao;
-import dao.StaffDao;
-
-public interface DaoFactory {
-    ComputerDao createComputerDao();
-    StaffDao createStaffDao();
-}
--- Assignment/Assignment5 AbstractFactory_Singleton/src/dao/StaffDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import bean.Staff;
-
-public interface StaffDao {
-    public int insertStaff(Staff staff);
-    public int deleteStaff(int id);
-    public int updateStaff(int id);
-}
